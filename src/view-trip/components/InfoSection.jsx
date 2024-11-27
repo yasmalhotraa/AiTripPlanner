5caf4545b58e8bdf6bd2007645244a76f8f2d227
yasmalhotraa <yashmalhotra330@gmail.com>
2024-11-28 05:17:59 +0530

    maxwidth

--- src/view-trip/components/InfoSection.jsx
+++ src/view-trip/components/InfoSection.jsx
@@ -12,7 +12,7 @@ function InfoSection({ trip }) {
       <div className="flex justify-between items-center">
         <div className="my-5 flex flex-col gap-2">
           <h2 className="font-bold text-xl">{trip?.userSelection?.location}</h2>
-          <div className="flex gap-5 max-500:flex-col max-500:gap-3 ">
+          <div className="flex gap-5 max-480:flex-col max-480:gap-3 ">
             <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500 ">
               📅 {trip?.userSelection?.noOfDays}{" "}
               {trip?.userSelection?.noOfDays == 1 ? "Day" : "Days"}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        "max-600": { max: "600px" },
+        "max-480": { max: "480px" },
       },
       borderRadius: {
         lg: "var(--radius)",
