133a767d1399905bf9a87726ca9bfea3cb5dddc1
yasmalhotraa <yashmalhotra330@gmail.com>
2024-11-28 18:08:54 +0530

    mt8

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="my-7 border-t-2 border-b-2 ">
+    <div className="mt-8 border-t-2 border-b-2 ">
       <h2 className="text-center p-2 text-gray-500">
         Created By Yash Malhotra AI Trip Planner App
       </h2>
