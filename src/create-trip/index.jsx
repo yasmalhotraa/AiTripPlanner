19a3088d956e0188319753bf0cd0ec54e37b47f0
yasmalhotraa <yashmalhotra330@gmail.com>
2024-11-30 18:33:54 +0530

    noOfDays

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -64,7 +64,7 @@ function CreateTrip() {
       setLoading(false); // Set loading to false if validation fails
       return;
     }
-    if (formData?.noOfDays > 10) {
+    if (formData?.noOfDays > 15) {
       toast("Please fill Days less than 10.");
       setLoading(false); // Set loading to false if validation fails
       return;
